ab28d4dbfd139d711172abd9ae81d7b41c8baa0e
NadezhdaT <kybalibri@mail.ru>
2024-10-23 01:22:37 +0700

    change tests

--- gh.test.js
+++ gh.test.js
@@ -13,54 +13,52 @@ describe("Github page tests", () => {
     await page.goto("https://github.com/team");
   });
   test("The h1 header content'", async () => {
-    jest.setTimeout(7000);
+    
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
-  });
+    expect(title2).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub");
+  }, 7000);
 
   test("The first link attribute", async () => {
-    jest.setTimeout(7000);
+    
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 7000);
 
   test("The page contains Sign in button", async () => {
-    jest.setTimeout(7000);
+    
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
-    });
+    },7000);
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
-    expect(actual).toContain("Sign up for free");
+    expect(actual).toContain("  Get started with Team");
   });
 });
 
 test("Title on GitHab Copilot page", async () => {
   await page.goto("https://github.com/features/copilot");
-  jest.setTimeout(7000);
+ 
 
   const title = await page.title();
   const expected = "GitHub Copilot · Your AI pair programmer · GitHub";
   expect(title).toEqual(expected);
-});
+}, 10000);
 
 test("Title on CI/CD page", async () => {
   await page.goto("https://github.com/solutions/use-case/ci-cd");
-  jest.setTimeout(7000);
-
+  
   const title = await page.title();
   const expected = "A Complete CI/CD Solution | GitHub · GitHub";
   expect(title).toEqual(expected);
-});
+}, 7000);
 
 test("Title on price page", async () => {
   await page.goto("https://github.com/pricing");
-  jest.setTimeout(7000);
-
+  
   const title = await page.title();
   const expected = "Pricing · Plans for every developer · GitHub";
   expect(title).toEqual(expected);
-});
+}, 7000);
